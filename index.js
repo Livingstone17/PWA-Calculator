e8e468a3c6e79461299763c8a2570bfcdc30e11c
Livingstone17 <peterson.omobolaji@gmail.com>
2022-01-10 18:52:41 +0100

    Developed calc with basic functionalities

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+const display = document.querySelector('.display');
+
+const Buttons = document.querySelector('.controls').children;
+
+const allSymbols = ['+', '-', "%", "C", "=", "X",, "<-"];
+
+let firstVal = '';
+let secondVal = '';
+let symbol = '';
+let result = '';
+
+const calculate = () => {
+    firstVal = parseFloat(firstVal);
+    secondVal = parseFloat(secondVal);
+
+    if(symbol === '+') result = firstVal + secondVal;
+    if(symbol === '-') result = firstVal - secondVal;
+    if(symbol === '/') result = firstVal / secondVal;
+    if(symbol === '-') result = firstVal - secondVal
+    if(symbol === 'X') result = firstVal * secondVal;
+
+    display.innerText = result;
+    firstVal = result;
+    secondVal = '';
+
+}
+
+// const backSpace = () => {
+   
+    
+// }
+for(let button of Buttons){
+    button.addEventListener('click', ()=>{
+        const {innerText: btnValue} = button;
+        const btnValueIsSymbol = allSymbols.includes(btnValue);
+
+        if (btnValue === "<-"){
+            // display.innerText.value.slice(0, display.innerText.value -1)
+            firstVal = secondVal = symbol = ''
+            var displayedValue = display.innerText;
+           
+            displayedValue = ''
+            // displayedValue.slice(0,- 1);
+            console.log("backspace clicked")
+        }
+
+        if (!secondVal && btnValue === '=') return null
+        if(btnValue === 'C'){
+            firstVal = secondVal = symbol = ''
+            return display.innerText = ''
+        }
+        if(firstVal && btnValueIsSymbol){
+            secondVal && calculate()
+            symbol = btnValue
+        }
+        else if(!symbol) {
+            firstVal += btnValue;
+        }else if(symbol) secondVal += btnValue;
+        if(btnValue !== '=') display.innerText +=btnValue;
+        // display.innerText += btnValue;
+    })
+}
